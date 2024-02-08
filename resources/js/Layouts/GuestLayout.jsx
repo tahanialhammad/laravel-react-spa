a5482b71e62f6a54c4f5acd8bc09ffd325f586e2
tahanialhammad <tahani_alhammad@yahoo.com>
2024-02-08 22:00:29 +0100

    update gust nav

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -16,7 +16,7 @@ export default function Guest({ children }) {
               {flash.message && ( <FlashMessage color="green" className="">{flash.message}</FlashMessage> )} 
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div className="w-full p-8">
                 {children}
             </div>
         </div>
--- resources/js/Layouts/GuestNav.jsx
+++ resources/js/Layouts/GuestNav.jsx
@@ -1,8 +1,14 @@
+import React, { useState } from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import { Link } from "@inertiajs/react";
 import NavLink from "@/Components/NavLink";
 
 export default function GuestNav() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
     const guestNavLinks = [
         {
             id: 1,
@@ -22,38 +28,77 @@ export default function GuestNav() {
     ];
 
     return (
-        <nav className="bg-white border-b border-gray-100">
-            {/* navbar :  https://tailwindcomponents.com/component/responsive-navbar-12  */}
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16">
-                    <div className="flex">
-                        <div className="shrink-0 flex items-center">
-                            <Link href="/">
-                                <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                            </Link>
-                        </div>
-
-                        <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <NavLink
-                                href={route("welcome")}
-                                active={route().current("welcome")}
+        <nav className="bg-gray-800tt p-4">
+            <div className="max-w-7xl mx-auto md:flex justify-between items-center">
+                <div className="flex justify-between items-center">
+                    <Link href="/">
+                        <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                    </Link>
+                    <div className="md:hidden">
+                        <button
+                            onClick={toggleNavbar}
+                            className="text-white focus:outline-none"
+                        >
+                            <svg
+                                className="h-6 w-6 text-red-400"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
                             >
-                                Home
-                            </NavLink>
+                                {isOpen ? (
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                ) : (
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M4 6h16M4 12h16m-7 6h7"
+                                    />
+                                )}
+                            </svg>
+                        </button>
+                    </div>
+                </div>
 
-                            {guestNavLinks.map((guestNavLink) => (
-                                <NavLink key={guestNavLink.id}
+                <div
+                    className={`md:flex flex-grow justify-between items-center ${
+                        isOpen ? "block" : "hidden"
+                    }`}
+                >
+                    <ul className="md:flex items-center">
+                        {guestNavLinks.map((guestNavLink) => (
+                            <li key={guestNavLink.id}>
+                                <NavLink
                                     href={route(guestNavLink.route)}
                                     active={route().current(guestNavLink.route)}
                                 >
                                     {guestNavLink.name}
                                 </NavLink>
-                            ))}
-                        </div>
-                    </div>
+                            </li>
+                        ))}
+                    </ul>
 
-                    <div className="hidden sm:flex sm:items-center sm:ms-6">
-                        <div className="ms-3 relative">login btn</div>
+                    <div>
+                        <div className="md:flex items-center">
+                            <Link
+                                href={route("login")}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route("register")}
+                                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Register
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
