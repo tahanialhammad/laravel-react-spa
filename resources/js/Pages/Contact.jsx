2e8ce6fd1176c5a4a6bee3ef33e1dcd1e28aacf7
tahanialhammad <tahani_alhammad@yahoo.com>
2024-02-11 22:13:40 +0100

    update FramerMotion,  Text Animation, npm run build

--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -1,13 +1,30 @@
 import SiteLayout from "@/Layouts/SiteLayout";
 import { Link, Head } from "@inertiajs/react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Contact(props) {
     return (
         <SiteLayout>
             <Head title="Contact" />
-            <div className="">
-                <h1>Contact {props.name} page</h1>
-            </div>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    initial={{
+                        x: "-100vw",
+                    }}
+                    animate={{
+                        x: 0,
+                    }}
+                    transition={{
+                        duration: 1.5,
+                    }}
+                    exit={{
+                        y: "-100vh",
+                    }}
+                    className="bg-yellow-300 relative sm:flex sm:justify-center sm:items-center min-h-screen"
+                >
+                    <h1>Contact {props.name} page</h1>
+                </motion.div>
+            </AnimatePresence>
         </SiteLayout>
     );
 }
