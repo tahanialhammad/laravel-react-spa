14829c857c26b3a8c9b6c294a6be86ad8f78657d
tahanialhammad <tahani_alhammad@yahoo.com>
2024-03-06 20:45:12 +0100

    update

--- resources/js/Pages/Home/partials/HeroSection.jsx
+++ resources/js/Pages/Home/partials/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-
+import Winner from "./Winner";
 
 export default function HeroSection() {
     return (
@@ -11,27 +11,72 @@ export default function HeroSection() {
                     className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                     aria-hidden="true"
                 >
-                      
                     <motion.div
-                      initial={{ x: 0 }}
-                      animate={{ x: 900 }}
-                      transition={{
-                          duration: 7.5,
-                          repeat: Infinity,
-                          repeatType: "reverse",
-                      }}
+                        initial={{ x: 0 }}
+                        animate={{ x: 900 }}
+                        transition={{
+                            duration: 7.5,
+                            repeat: Infinity,
+                            repeatType: "reverse",
+                        }}
                         className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                         style={{
                             clipPath:
                                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
                         }}
-                    >
-                    </motion.div>
+                    ></motion.div>
                 </div>
+                <div className="flex flex-row w-full">
+                    <div className="w-2/3 text-justify ">
+                        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                            <div className="text-left">
+                                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                                    Tahani Alhammad Webontwikkelaar
+                                </h1>
+                                <p className="mt-6 text-lg leading-8 text-gray-600">
+                                    Mijn passie ligt in het creëren van
+                                    websites, waarbij ik me specifiek richt op
+                                    de front-end ontwikkeling van
+                                    Laravel-applicaties en en het maken van
+                                    WordPress websites voor MKB. Bent u op zoek
+                                    naar een betrouwbare en getalenteerde
+                                    front-end developer voor uw onderneming?
+                                    Zoek niet verder. Met mijn diverse ervaring
+                                    en up-to-date kennis van de nieuwste
+                                    technologieën, ben ik in staat om voor uw
+                                    bedrijf een hoogwaardige en
+                                    gebruiksvriendelijke front-end te
+                                    realiseren. Neem vandaag nog contact met mij
+                                    op en ontdek hoe ik uw bedrijf kan
+                                    transformeren!
+                                </p>
+                                <div className="mt-10 flex items-center justify-center gap-x-6">
+                                    <a
+                                        href="#"
+                                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Get started
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="text-sm font-semibold leading-6 text-gray-900"
+                                    >
+                                        Learn more{" "}
+                                        <span aria-hidden="true">→</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-1/3 overflow-hidden">
+                        <Winner />
+                    </div>
+                </div>
+
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                           Tahanina webdevelopment.{" "}
+                            Tahanina webdevelopment.{" "}
                             <a
                                 href="https://www.linkedin.com/in/tahanialhammad/"
                                 className="font-semibold text-indigo-600"
@@ -46,12 +91,21 @@ export default function HeroSection() {
                     </div>
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                            Data to enrich your online business
+                            Tahani Alhammad Webontwikkelaar
                         </h1>
                         <p className="mt-6 text-lg leading-8 text-gray-600">
-                            Anim aute id magna aliqua ad ad non deserunt sunt.
-                            Qui irure qui lorem cupidatat commodo. Elit sunt
-                            amet fugiat veniam occaecat fugiat aliqua.
+                            Mijn passie ligt in het creëren van websites,
+                            waarbij ik me specifiek richt op de front-end
+                            ontwikkeling van Laravel-applicaties en en het maken
+                            van WordPress websites voor MKB. Bent u op zoek naar
+                            een betrouwbare en getalenteerde front-end developer
+                            voor uw onderneming? Zoek niet verder. Met mijn
+                            diverse ervaring en up-to-date kennis van de
+                            nieuwste technologieën, ben ik in staat om voor uw
+                            bedrijf een hoogwaardige en gebruiksvriendelijke
+                            front-end te realiseren. Neem vandaag nog contact
+                            met mij op en ontdek hoe ik uw bedrijf kan
+                            transformeren!
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <a
