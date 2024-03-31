f0ec7deecce7aa769b69d7c0a0b5a31b3db74b49
tahanialhammad <tahani_alhammad@yahoo.com>
2024-03-31 16:19:05 +0200

    update

--- resources/js/Pages/Admin/Dashboard/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard/Dashboard.jsx
@@ -16,7 +16,7 @@ export default function Dashboard({ auth }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-red-400 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="bg-white dark:bg-green-400 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">You're logged in!
                         </div>
                     </div>
