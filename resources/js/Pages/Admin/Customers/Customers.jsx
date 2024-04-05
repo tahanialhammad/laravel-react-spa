4ab5908a9716bdc7fc1c36758cf2428ae0239518
tahanialhammad <tahani_alhammad@yahoo.com>
2024-04-05 19:05:00 +0200

    update

--- resources/js/Pages/Admin/Customers/Customers.jsx
+++ resources/js/Pages/Admin/Customers/Customers.jsx
@@ -1,66 +1,53 @@
+import DataGridTable from "@/Components/MUI/DataGridTable";
 import MiniDrawerAuthLayout from "@/Layouts/MiniDrawerAuthLayout";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { rows } from "./Data";
-import { useTheme } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 
-export default function Customers({ auth, users }) {
-    const theme = useTheme();
-    const columns = [
-        { field: "id", headerName: "Id", width: 33 },
-        { field: "name", headerName: "User name", flex: 1 },
-        { field: "email", headerName: "User email", flex: 1 },
-        { field: "adress", headerName: "Adress", flex: 1 },
-        { field: "post", headerName: "Post", flex: 1 },
-
-        {
-            field: "role",
-            headerName: "role",
-            flex: 1,
-            align: "center",
-            headerAlign: "center",
-            renderCell: ({ row: { role } }) => {
-                return (
-                    <Box
-                        sx={{
-                            p: "5px",
-                            borderRadius: "5px",
-                            display: "flex",
-                            backgroundColor:
-                                role === "admin"
-                                    ? theme.palette.primary.dark
-                                    : "red",
-                        }}
-                    >
-                        <Typography variant="body1"> {role}</Typography>
-                    </Box>
-                );
-            },
+const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "User name", flex: 1 },
+    { field: "email", headerName: "User email", flex: 1 },
+    {
+        field: "is_admin",
+        headerName: "Role",
+        flex: 1,
+        align: "center",
+        headerAlign: "center",
+        renderCell: ({ row: { is_admin } }) => {
+            return (
+                <Box
+                    sx={{
+                        p: "5px",
+                        borderRadius: "5px",
+                        display: "flex",
+                        backgroundColor: is_admin ? "green" : "red",
+                    }}
+                >
+                    <Typography variant="body1">
+                        {" "}
+                        {is_admin ? "Admin" : "User"}
+                    </Typography>
+                </Box>
+            );
         },
-    ];
+    },
+];
 
+export default function Customers({ auth, users }) {
     return (
         <MiniDrawerAuthLayout user={auth.user}>
             <div>Customers</div>
 
-            <Box sx={{ height: 300, width: "100%" }}>
-                <DataGrid
+            <DataGrid
                 checkboxSelection
                 // filters
-                    slots={{
-                        toolbar: GridToolbar,
-                    }}
-                    rows={rows}
-                    columns={columns}
-                />
-            </Box>
-
-            <ul>
-                {users.map((user) => (
-                    <li>{user.name}</li>
-                ))}
-            </ul>
+                slots={{
+                    toolbar: GridToolbar,
+                }}
+                rows={users}
+                columns={columns}
+            />
         </MiniDrawerAuthLayout>
     );
 }
