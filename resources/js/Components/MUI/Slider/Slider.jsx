dde5b4289f6ca615cd4ef899d1fc92c8cabd2410
tahanialhammad <tahani_alhammad@yahoo.com>
2024-05-26 17:03:45 +0200

    update

--- resources/js/Components/MUI/Slider/Slider.jsx
+++ resources/js/Components/MUI/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -10,6 +10,7 @@ import "swiper/css/pagination";
 import "./style.css";
 
 import { Pagination } from "swiper/modules";
+import Verticalslider from "./Verticalslider";
 
 export default function Slider() {
     return (
@@ -17,6 +18,7 @@ export default function Slider() {
             sx={{
                 // mt:2.5
                 display: "flex",
+                height: "60vh",
             }}
         >
             {/* slider in mui + swiperjs : https://swiperjs.com/react */}
@@ -28,13 +30,17 @@ export default function Slider() {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src=".\images\bg7104.jpg" alt="" />
+                    <img src=".\images\bg7073.jpg" alt="" />
                     <Box
-                        sx={{ position: "absolute", right: "10%" }}
+                        sx={{
+                            position: "absolute",
+                            left: "5%",
+                            textAlign: "left",
+                        }}
                         className="bordertt"
                     >
                         <Typography sx={{ color: "#222" }} variant="h5">
-                            Latsete Project
+                            Latest Project
                         </Typography>
                         <Typography
                             sx={{ color: "#222", fontWeight: 400, my: 1 }}
@@ -49,23 +55,42 @@ export default function Slider() {
                             alignItems={"center"}
                         >
                             <Typography sx={{ color: "#333" }} variant="h4">
-                                Handmade candel  
+                                Handmade bag
                             </Typography>
                             <Typography
-                                sx={{ color: "#cccc00", ml: 1 }}
+                                sx={{ color: "#ffc8dd", ml: 1 }}
                                 variant="h4"
                             >
                                 webdshop
                             </Typography>
                         </Stack>
+
+                        <Button
+                            sx={{
+                                px: 5,
+                                py: 1,
+                                mt: 2,
+                                backgroundColor: "#222",
+                                boxShadow: "0 4px 16px rgba(43, 52, 69, 0.1)",
+                                color: "#fff",
+                                borderRadius: "1px",
+                                "&:hover": {
+                                    bgcolor: "#151515",
+                                },
+                            }}
+                        >
+                            Go to webshop
+                        </Button>
                     </Box>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src=".\images\bg7073.jpg" alt="" />
+                    <img src=".\images\bg7104.jpg" alt="" />
                 </SwiperSlide>
             </Swiper>
 
-            <Box sx={{ width: "30%", margin: "10px" }}>Vertical slider</Box>
+            <Box sx={{ width: "30%", mx: 1 }}>
+                <Verticalslider />
+            </Box>
         </Container>
     );
 }
