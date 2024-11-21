3bdc69a259e65436bac0c0cb0b2aea0d2cb7c9f9
tahanialhammad <tahani_alhammad@yahoo.com>
2024-11-21 18:13:00 +0100

    update

--- resources/js/Pages/Home/Welcome.jsx
+++ resources/js/Pages/Home/Welcome.jsx
@@ -5,42 +5,12 @@ import FramerMotion from "@/Pages/FramerMotion/FramerMotion";
 import Features from "./partials/Features";
 import Categories from "./partials/Categories";
 
-export default function Welcome({ auth, products }) {
+export default function Welcome({ auth, products, initialTime }) {
     return (
         <SiteLayout user={auth.user}>
-            <div className="flex w-full gap-8">
-                {/* <div className="w-1/3 bg-lime-200 rounded-3xl p-4">
-    <img src="https://demo.phlox.pro/shop-baby/wp-content/uploads/sites/319/2021/06/Cute-Baby-1194x813-1.png" alt="" />
-</div> */}
-
-                <div className="w-2/5 bg-lime-200 rounded-3xl p-4 relative">
-                    <img
-                        src="https://demo.phlox.pro/shop-baby/wp-content/uploads/sites/319/2021/06/Cute-Baby-1194x813-1.png"
-                        alt=""
-                        className="rounded-3xl"
-                    />
-                    <div className="absolute inset-0 bg-gradient-to-t from-lime-200 via-transparent"></div>
-                </div>
-
-                <div className="w-3/5 flex flex-col gap-8">
-                    <div className="rounded-3xl">
-                        <img
-                            className="rounded-3xl"
-                            src="https://demo.phlox.pro/shop-baby/wp-content/uploads/sites/319/2021/06/Mask-Group-10.jpg"
-                            alt=""
-                        />
-                    </div>
-
-                    <div className="">
-                        <img
-                            className="rounded-3xl"
-                            src="https://demo.phlox.pro/shop-baby/wp-content/uploads/sites/319/2021/06/Mask_Group_9.png"
-                            alt=""
-                        />
-                    </div>
-                </div>
-            </div>
-
+            <HeroSection />
+        
+            <Categories initialTime={initialTime} />
             <div className="flex w-full gap-8 my-10">
                 <div
                     className="w-1/4 bg-cover bg-center text-black flex flex-col items-center justify-centertt rounded-3xl p-4"
